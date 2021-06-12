a9ed2c5b53382b7950f9bda521f5681c75ca7e78
Andrew Sidesinger <sidesinger@gmail.com>
2021-06-12 11:13:57 -0400

    Test updates

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -8,7 +8,7 @@ namespace HighSchoolSelector
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.55560);
 
         public string Summary { get; set; }
     }
