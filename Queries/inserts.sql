7e7e15b3d45ab58e03b2e2feab90a3019b47f6ba
Andrew Sidesinger <sidesinger@gmail.com>
2021-06-12 12:56:04 -0400

    add insert sql

---
+++ Queries/inserts.sql
@@ -0,0 +1,6 @@
+use sandbox;
+insert into StudentProfile (StudentId, FirstName,LastName,GradePointAverage,EntranceExamScore)
+values (1,'Toe','Moss',3.4,99);
+
+insert into StudentProfile (StudentId, FirstName,LastName,GradePointAverage,EntranceExamScore)
+values (2,'And','Rew',1.0,22);
